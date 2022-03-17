8ec2bea1a4302ce96017a243d91176f7b27d9c7f
Daria <dveremina@edu.hse.ru>
2022-03-18 00:11:04 +0300

    code and setup cleanup

--- git_info.sh
+++
@@ -1,1 +0,0 @@
-python3 git_info/main.py -repo=./repo -url=https://github.com/scikit-learn/scikit-learn -data=./
--- git_info/main.py
+++ git_info/main.py
@@ -4,18 +4,18 @@ from git_summarizer import process_repo
 
 
 @click.command()
-@click.option("--repo-dir", "-repo")
-@click.option("--repo-url", "-url", default=None)
-@click.option("--data-dir", "-data")
-def get_input(repo_dir: str, repo_url: str, data_dir: str):
+@click.option("--repo-dir", "-r")
+@click.option("--repo-url", "-u", default=None)
+@click.option("--output-file", "-o")
+def get_input(repo_dir: str, repo_url: str, output_file: str):
     """
     CLI to receive input with necessary paths to repository and directories
-    :param repo_dir:    directory with local repository
-    :param repo_url:    url of remote repository
-    :param data_dir:    directory to save output data
+    :param repo_dir: directory with local repository
+    :param repo_url: url of remote repository
+    :param output_file: path to file for output data
     """
-    click.echo(f"Processing repository from {repo_dir} and saving data to {data_dir}")
-    process_repo(repo_dir, data_dir, repo_url)
+    click.echo(f"Processing repository from {repo_dir} and saving data to {output_file}")
+    process_repo(repo_dir, output_file, repo_url)
 
 
 if __name__ == "__main__":
