cc055f291b887ddb42d39aef25b73f729581380f
Daria <dveremina@edu.hse.ru>
2022-06-21 19:14:22 +0300

    add tree-sitter downloads

--- git_info/tree_sitter_utils.py
+++ git_info/tree_sitter_utils.py
@@ -1,14 +1,54 @@
+import platform
+import urllib.request
+from os.path import abspath, dirname, join
 from typing import Dict
 
 from tree_sitter import Language
+
+URLS = {
+    "Linux":
+        "https://github.com/d-eremina/2022_similar_dev_search_eremina/releases/latest/download/languages-linux.so",
+    "Darwin":
+        "https://github.com/d-eremina/2022_similar_dev_search_eremina/releases/latest/download/languages-darwin.so"
+}
+
+FILENAMES = {
+    "Linux": "languages-linux.so",
+    "Darwin": "languages-darwin.so"
+}
+
+
+def _get_file_name() -> str | None:
+    if platform.system() in FILENAMES:
+        return FILENAMES[platform.system()]
+    else:
+        raise OSError(f"{platform.system()} is not supported in current version")
+
+
+def _get_url() -> str | None:
+    if platform.system() in URLS:
+        return URLS[platform.system()]
+    else:
+        raise OSError(f"{platform.system()} is not supported in current version")
+
+
+def download_languages() -> None:
+    urllib.request.urlretrieve(
+        _get_url(),
+        abspath(join(abspath(join(dirname(__file__), "build")), _get_file_name())))
+
+
+download_languages()
+
+FILENAME = _get_file_name()
 
 """
 Supported languages for parsing
 """
 LANGUAGES: Dict[str, Language] = {
-    "python": Language("./build/languages.so", "python"),
-    "java": Language("./build/languages.so", "java"),
-    "javascript": Language("./build/languages.so", "javascript")
+    "python": Language(f"./build/{FILENAME}", "python"),
+    "java": Language(f"./build/{FILENAME}", "java"),
+    "javascript": Language(f"./build/{FILENAME}", "javascript")
 }
 
 """
